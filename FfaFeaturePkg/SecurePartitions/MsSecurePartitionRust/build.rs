88a4c720218fc7128804f3a863fdb551828b5c9a
Kun Qin <kuqin@microsoft.com>
2025-08-13 17:41:06 -0700

    FfaFeaturePkg: MsSecurePartitionRust: Add rust based secure partition
    This change adds a rust based secure partition that hosts both test
    service and inter-partition service.
    
    Signed-off-by: Kun Qin <kun.qin@microsoft.com>

---
+++ FfaFeaturePkg/SecurePartitions/MsSecurePartitionRust/build.rs
@@ -0,0 +1,13 @@
+// Copyright 2025 Microsoft Corporation
+// This project is dual-licensed under Apache 2.0 and MIT terms.
+// See LICENSE-APACHE and LICENSE-MIT for details.
+
+fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "none" {
+        println!("cargo:rustc-env=BUILD_TIME={}", chrono::Utc::now().to_rfc3339());
+        println!("cargo:rustc-link-arg=-TFfaFeaturePkg/SecurePartitions/MsSecurePartitionRust/linker/image.ld");
+        println!("cargo:rustc-link-arg=-TFfaFeaturePkg/SecurePartitions/MsSecurePartitionRust/linker/qemu.ld");
+        println!("cargo:rerun-if-changed=FfaFeaturePkg/SecurePartitions/MsSecurePartitionRust/linker/qemu.ld");
+        println!("cargo:rerun-if-changed=FfaFeaturePkg/SecurePartitions/MsSecurePartitionRust/linker/image.ld");
+    }
+}
